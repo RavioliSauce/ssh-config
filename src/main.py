10c170553e116d7eb77b7dffc0480c7bac443bc2
ravioliboy <rgtarp@gmail.com>
2024-08-01 21:09:51 -0500

    chore: Add GUI functionality to main.py (hello world)

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
-pass
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+        
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+    
+    sys.exit(app.exec())
